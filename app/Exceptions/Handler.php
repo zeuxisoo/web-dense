0e23703818e4e479ebaf0a9d7fe9b5875ccb375b
Zeuxis Lo <seekstudio@gmail.com>
2015-05-15 23:27:39 +0800

    remove whoops exception library

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,16 +42,7 @@ class Handler extends ExceptionHandler {
             $controller->index();
             exit;
         }else{
-            // return parent::render($request, $e);
-
-            $whoops = new \Whoops\Run;
-            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-
-            return new \Illuminate\Http\Response(
-                $whoops->handleException($e),
-                $e->getStatusCode(),
-                $e->getHeaders()
-            );
+            return parent::render($request, $e);
         }
     }
 
