e0f4e448c45f54af351a79ae257512380653321a
Zeuxis Lo <seekstudio@gmail.com>
2015-05-15 23:07:13 +0800

    add user sign up api, user model and user table migration

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,12 +1,29 @@
 <?php
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller {
 
-    public function signup() {
-        return response()->json($this);
+    public function signup(Request $request) {
+        $form_data = $request->all();
+        $validator = Validator::make($form_data, [
+            'username'              => 'required|alpha_dash|min:6|max:30|unique:users',
+            'email'                 => 'required|email|unique:users',
+            'password'              => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }else{
+            $user = User::create($form_data);
+
+            return response()->json($user);
+        }
     }
 
 }
