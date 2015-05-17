3a7bbea50aed6d2b5b24da1236cba62daffa4bd2
Zeuxis Lo <seekstudio@gmail.com>
2015-05-17 18:19:56 +0800

    add toastr service

--- public/asset/client/js/services.js
+++ public/asset/client/js/services.js
@@ -11,3 +11,27 @@ denseApp.factory('restAPI', ['$resource', function($resource) {
         user: $resource('/api/user/:action')
     }
 }]);
+
+denseApp.factory('toast', ['$log', 'JSONKit', function($log, JSONKit) {
+    var toast   = {};
+
+    angular.forEach(['info', 'error', 'success', 'warning'], function (method) {
+        toast[method] = function (message, title) {
+            var log     = $log[method] || $log.log,
+                title   = JSONKit.toStr(title),
+                message = angular.isObject(message) ? angular.toJson(message) : JSONKit.toStr(message);;
+
+            log(message, title);
+
+            toastr[method](message, title);
+        };
+    });
+
+    toastr.options = angular.extend({
+        positionClass: 'toast-bottom-full-width'
+    }, toast.options);
+
+    toast.clear = toastr.clear;
+
+    return toast;
+}]);
